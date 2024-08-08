3b843024277b35018e60cb4031da145a4b4d6cd0
DamoFD <voshall@yahoo.com>
2024-08-08 16:09:52 -0700

    merge attributes

--- core/template/attributes.go
+++ core/template/attributes.go
@@ -4,6 +4,13 @@ import (
 	"regexp"
 )
 
+// A map of key value pairs for attributes
+//
+// ex: map[string]string{"class": "btn", "type": "submit",}
+//
+// Since: 0.1.0
+type Attributes map[string]string
+
 // Get the attributes from the specified element
 //
 // Params:
@@ -13,8 +20,8 @@ import (
 // - map[string]string: attribute key value pairs
 //
 // Since: 0.1.0
-func getAttributes(element string) map[string]string {
-	attributes := make(map[string]string)
+func getAttributes(element string) Attributes {
+	attributes := make(Attributes)
 
 	regex := regexp.MustCompile(`(\w+)="([^"]*)"`)
 	matches := regex.FindAllStringSubmatch(element, -1)
@@ -26,4 +33,29 @@ func getAttributes(element string) map[string]string {
 	}
 
 	return attributes
+}
+
+// Merges another Attributes map into the current one.
+// If a key exists in both maps, the other map will override.
+//
+// Receiver:
+// a (Attributes): current attributes map
+//
+// Params:
+// - other (Attributes): attributes to be added
+//
+// Returns:
+// - Attributes: combined attributes map
+//
+// Since: 0.1.0
+func (a Attributes) mergeAttributes(other Attributes) Attributes {
+	for key, value := range other {
+		if key == "class" {
+			a[key] = a[key] + " " + value
+		} else {
+			a[key] = value
+		}
+	}
+
+	return a
 }
--- core/template/attributes_test.go
+++ core/template/attributes_test.go
@@ -21,3 +21,52 @@ func TestGetAttributes(t *testing.T) {
 		}
 	}
 }
+
+func TestMergeAttributes(t *testing.T) {
+	original := make(Attributes)
+	original["class"] = "btn"
+	original["type"] = "submit"
+
+	toMerge := make(Attributes)
+	toMerge["class"] = "primary"
+	toMerge["id"] = "button"
+
+	expected := map[string]string{
+		"class": "btn primary",
+		"type":  "submit",
+		"id":    "button",
+	}
+
+	result := original.mergeAttributes(toMerge)
+
+	for key, expectedValue := range expected {
+		if value, exists := result[key]; !exists || value != expectedValue {
+			t.Errorf("Expected attribute %s=\"%s\", but got %s\"%s\"", key, expectedValue, key, value)
+		}
+	}
+}
+
+func TestMergeAttributesOverwrite(t *testing.T) {
+	original := make(Attributes)
+	original["class"] = "btn"
+	// should be overwritten
+	original["id"] = "submit"
+
+	toMerge := make(Attributes)
+	toMerge["class"] = "primary"
+	// should overwrite submit id
+	toMerge["id"] = "button"
+
+	expected := map[string]string{
+		"class": "btn primary",
+		"id":    "button",
+	}
+
+	result := original.mergeAttributes(toMerge)
+
+	for key, expectedValue := range expected {
+		if value, exists := result[key]; !exists || value != expectedValue {
+			t.Errorf("Expected attribute %s=\"%s\", but got %s\"%s\"", key, expectedValue, key, value)
+		}
+	}
+}
